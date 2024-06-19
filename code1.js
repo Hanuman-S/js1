6263f92f5df7d0ba054d1119144fd394368e33b1
Hanuman-S <hanuman212006@gmail.com>
2024-06-19 16:18:08 +0530

    first commit

--- code1.js
+++ code1.js
@@ -9,3 +9,7 @@ const student = {
 };
 
 console.log(student);
+
+/* var is old and should not be used, but it can be updated and re-declared, it has global scope */
+/* let can be updated but not re-declared, it has block scope */
+/* const can be not be updated and not re-declared, it has block scope */
